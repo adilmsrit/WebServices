d550784aecba4d9566268d31d8a292046c07c63a
Mohammed.Nazeer <mohammed.nazeer@emaratech.ae>
2019-01-18 21:58:03 +0400

    Added getValueByJPath method in Utils class.

--- src/main/java/WebService/Base/BaseClass.java
+++ src/main/java/WebService/Base/BaseClass.java
@@ -10,6 +10,14 @@ public class BaseClass {
 
     public Properties prop;
 
+
+    public int RESPONSE_STATUS_CODE_200 = 200;
+    public int RESPONSE_STATUS_CODE_500 = 500;
+    public int RESPONSE_STATUS_CODE_400 = 400;
+    public int RESPONSE_STATUS_CODE_401 = 401;
+    public int RESPONSE_STATUS_CODE_201 = 201;
+
+
     public BaseClass() {
         try {
             prop = new Properties();
--- src/test/java/WebService/GetAPITest.java
+++ src/test/java/WebService/GetAPITest.java
@@ -6,6 +6,7 @@ import org.apache.http.Header;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.util.EntityUtils;
 import org.json.JSONObject;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -33,6 +34,9 @@ public class GetAPITest extends BaseClass {
         //a. Get the Status code.
         int statusCode = closeableHttpResponse.getStatusLine().getStatusCode();
         System.out.println("Status code --> " + statusCode);
+
+        Assert.assertEquals(statusCode, RESPONSE_STATUS_CODE_200, "Status code is not 200");
+
 
         //b. Json String:
         String responseString = EntityUtils.toString(closeableHttpResponse.getEntity(), "UTF-8");
