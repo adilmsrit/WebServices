4ddd765641e8dca107ada99d6c43bf68a7cac25e
Mohammed.Nazeer <mohammed.nazeer@emaratech.ae>
2019-01-26 22:44:39 +0400

    Adding httpPost.

--- src/main/java/WebService/Base/RestClient.java
+++ src/main/java/WebService/Base/RestClient.java
@@ -4,6 +4,8 @@ package WebService.Base;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 
@@ -16,7 +18,7 @@ public class RestClient {
     CloseableHttpResponse closeableHttpResponse;
 
     //Get Method
-    public CloseableHttpResponse get(String url) throws ClientProtocolException , IOException {
+    public CloseableHttpResponse get(String url) throws ClientProtocolException, IOException {
 
         CloseableHttpClient httpClient = HttpClients.createDefault();
         HttpGet httpGet = new HttpGet(url);
@@ -25,16 +27,31 @@ public class RestClient {
         return closeableHttpResponse;
     }
 
-    //Get Method
-    public CloseableHttpResponse get(String url, HashMap<String, String> headerMap) throws ClientProtocolException , IOException{
+    //Get Method with Headers.
+    public CloseableHttpResponse get(String url, HashMap<String, String> headerMap) throws ClientProtocolException, IOException {
         CloseableHttpClient httpClient = HttpClients.createDefault();
         HttpGet httpGet = new HttpGet(url);
 
-        for (Map.Entry<String, String> entry : headerMap.entrySet()){
-             httpGet.addHeader(entry.getKey(),entry.getValue());
+        for (Map.Entry<String, String> entry : headerMap.entrySet()) {
+            httpGet.addHeader(entry.getKey(), entry.getValue());
         }
         closeableHttpResponse = httpClient.execute(httpGet);
 
+        return closeableHttpResponse;
+
+    }
+
+    public CloseableHttpResponse post (String url, String entityString, HashMap<String, String> headerMap) throws Exception{
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        HttpPost httpPost = new HttpPost(url);
+        httpPost.setEntity(new StringEntity(entityString));
+
+        //for Headers.
+        for (Map.Entry<String, String> entry : headerMap.entrySet()) {
+            httpPost.addHeader(entry.getKey(), entry.getValue());
+        }
+
+        closeableHttpResponse = httpClient.execute(httpPost);
         return closeableHttpResponse;
 
     }
