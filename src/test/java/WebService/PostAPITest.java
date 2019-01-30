93010d2879859262a18c768fae598ade2f053005
Mohammed.Nazeer <mohammed.nazeer@emaratech.ae>
2019-01-30 22:30:54 +0400

    Adding httpPost_2.

--- src/test/java/WebService/PostAPITest.java
+++ src/test/java/WebService/PostAPITest.java
@@ -5,6 +5,9 @@ import WebService.Base.RestClient;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import data.Users;
 import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONObject;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -25,19 +28,33 @@ public class PostAPITest extends BaseClass {
     }
 
     @Test
-    public void postAPITest() throws Exception{
+    public void postAPITest() throws Exception {
         restClient = new RestClient();
         HashMap<String, String> headerMap = new HashMap<String, String>();
         headerMap.put("Content-Type", "application/json");
 
-//Jackson API
+        //Jackson API
         ObjectMapper mapper = new ObjectMapper();
         Users users = new Users("morpheus", "leader");
 
         //object to json.
-        mapper.writeValue(new File("data/users.json"),users);
+        mapper.writeValue(new File("C:\\Users\\mohammed.nazeer\\IdeaProjects\\WebServices\\src\\main\\java\\data\\users.json"), users);
+        String userJsonString = mapper.writeValueAsString(users);
+
+        System.out.println("Outputs the Json Sting generated --> " + userJsonString);
+
+        closeableHttpResponse = restClient.post(url, userJsonString, headerMap);
+
+        //1. Get the status.
+        int statusCode = closeableHttpResponse.getStatusLine().getStatusCode();
+        System.out.println("statusCode --> " + statusCode);
+        Assert.assertEquals(statusCode, baseClass.RESPONSE_STATUS_CODE_201);
+
+        String responseString = EntityUtils.toString(closeableHttpResponse.getEntity(), "UTF-8");
+        JSONObject jsonObject = new JSONObject(responseString);
+        System.out.println("jsonObject --> "+jsonObject);
+
 
 
     }
-
 }
